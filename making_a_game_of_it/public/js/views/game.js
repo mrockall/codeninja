c894f20675348f1a9d59a6b257da43a4551d7a5e
Mike Rockall <mrockall@gmail.com>
2013-11-16 13:43:08 +0000

    Added a countdown animation for the game start.

--- making_a_game_of_it/public/js/views/game.js
+++ making_a_game_of_it/public/js/views/game.js
@@ -12,17 +12,52 @@ app.Views.Game = Backbone.View.extend({
     player_template: _.template( $( '#player_game_screen' ).html() ),
 
     initialize: function() {
-      console.log('Here');
     },
 
     render: function() {
-
       if(app.Game.get('role') == 'Host') {
         this.$el.html( this.host_template() );
       } else {
         this.$el.html( this.player_template() );
       }
 
+      this.countdown();
       return this;
+    },
+
+    countdown: function() {
+      var start_time = 5,
+          $countdown = this.$el.find(".countdown"),
+          $count = $countdown.find(".count");
+
+      $countdown.fadeIn(200);
+
+      $count.text(start_time);
+      setTimeout(_.bind(function(){
+        $count.addClass('huge');
+      }, this), 10);
+
+      // Start a 1 second timer
+      var timer = setInterval(_.bind(countItDown, this), 1100);
+
+      // Decrement the displayed timer value on each 'tick'
+      function countItDown(){
+
+        start_time -= 1
+
+        if( start_time <= 0 ){
+          // Stop the timer and do the callback.
+          clearInterval(timer);
+          this.$el.find('.countdown').fadeOut();
+          // game.IO.socket.emit('hostCountdownFinished', game.get("game_id"));
+          return;
+        }
+
+        $count.text(start_time);
+        $count.removeClass('huge');
+        setTimeout(_.bind(function(){
+          $count.addClass('huge');
+        }, this), 100);
+      }
     }
 });
