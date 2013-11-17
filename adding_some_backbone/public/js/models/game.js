0a16608fcfb59a92eefd2c22959ed9f08b4cf5b9
Mike Rockall <mrockall@gmail.com>
2013-11-17 23:34:03 +0000

    Added some more comments.

--- adding_some_backbone/public/js/models/game.js
+++ adding_some_backbone/public/js/models/game.js
@@ -4,6 +4,8 @@ if(_.isUndefined(app.Models)) app.Models = {};
 
 /**
  * Game Model
+ * This model holds all of our logic for our game.
+ * This model is also our interaction point for our websocket.
  */
 app.Models.Game = Backbone.Model.extend({
   defaults: {
@@ -11,19 +13,37 @@ app.Models.Game = Backbone.Model.extend({
     role: ''
   },
 
+  /**
+   * Init this model.
+   * Define some events to listen to from the server.
+   * @return {Object} A reference to itself
+   */
   initialize: function() {
     _.bindAll(this, 'onConnected', 'onNewGameCreated', 'playerJoinedRoom');
     
     io.socket.on('connected', this.onConnected );
     io.socket.on('host:new_game_created', this.onNewGameCreated );
     io.socket.on('player:joined_room', this.playerJoinedRoom );
+
+    return this;
   },
 
+  /**
+   * Event Hander 'connected'
+   * We have successfully opened a websocket connection
+   * Lets store the socket id
+   */
   onConnected: function() {
     this.set('socket_id', io.socket.socket.sessionid);
     console.log("== Successfully opened a socket connection, my_socket_id = " + this.get('socket_id'));
   },
 
+  /**
+   * Event Handler 'host:new_game_created'
+   * We have created a new game, need to set our role as a host
+   * and store the ID of the game.
+   * @param  {Object} data Object containing the game ID
+   */
   onNewGameCreated: function(data) {
     console.log("== Game created", data);
 
@@ -35,6 +55,11 @@ app.Models.Game = Backbone.Model.extend({
     Backbone.Events.trigger("game:created");
   },
 
+  /**
+   * Event Handler 'player:joined_room'
+   * A player has joined the room, lets add him to the collection
+   * @param  {Object} data Object containing data about the player
+   */
   playerJoinedRoom: function(data) {
     console.log("== Player Joined", data);
 
@@ -46,9 +71,5 @@ app.Models.Game = Backbone.Model.extend({
     } else {
       app.Router.navigate('waiting', {trigger: true});
     }
-  },
-
-  gameCountdown: function() {
-    console.log("== Time to begin the game!");
   }
 });
