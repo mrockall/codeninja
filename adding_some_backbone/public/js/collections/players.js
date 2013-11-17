aa602eae9530cb59b529035c605c8aa0246e6af6
Mike Rockall <mrockall@gmail.com>
2013-11-17 23:34:46 +0000

    Removed some logic from the Players collection.

--- adding_some_backbone/public/js/collections/players.js
+++ adding_some_backbone/public/js/collections/players.js
@@ -2,16 +2,5 @@ var app = app || {};
 if(_.isUndefined(app.Collections)) app.Collections = {};
 
 app.Collections.Players = Backbone.Collection.extend({
-    model: app.Models.Player,
-    players_per_game: 2,
-
-    initialize: function() {
-      this.on('add', this.game_start, this);
-    },
-
-    game_start: function() {
-      if(this.length == this.players_per_game){
-        Backbone.Events.trigger('game:countdown');
-      }
-    }
+    model: app.Models.Player
 });
